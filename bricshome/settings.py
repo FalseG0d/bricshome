673370dd306dec4fc82a3063d468885a59b5c51c
Apoorv Garg <u.garg.10@gmail.com>
2020-06-29 12:27:18 +0530

    Go Live

--- bricshome/settings.py
+++ bricshome/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#=jza++8@8=ljd_6*-^t%z8mt5uis4ihn2+--!miv8j9uc^)!l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bricshome.herokuapp.com','127.0.0.1']
 
 
 # Application definition
