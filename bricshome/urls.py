53cee651bb97d3d7b66622390c8d2730e665c24a
Apoorv Garg <u.garg.10@gmail.com>
2020-06-29 12:29:19 +0530

    Go Live

--- bricshome/urls.py
+++ bricshome/urls.py
@@ -23,5 +23,7 @@ urlpatterns = [
     path('',include('Home.urls')),
     path('content/',include('ContentWriter.urls')),
     path('profile/',include('Profile.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
