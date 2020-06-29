66bc5607ae85f7849149dc31517f859ff7e2adf5
Apoorv Garg <u.garg.10@gmail.com>
2020-06-29 12:34:16 +0530

    Go Live

--- bricshome/urls.py
+++ bricshome/urls.py
@@ -18,6 +18,9 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.static import serve 
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls,name="admin"),
     path('',include('Home.urls')),
@@ -26,4 +29,3 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
-urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
